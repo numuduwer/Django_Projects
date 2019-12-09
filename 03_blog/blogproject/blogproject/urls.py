045cc7b47bff0e1d6daa7cc6b2b11bb1193bc1bd
numuduwer <rlatlsshwkd@gmail.com>
2019-12-09 19:01:43 +0900

    url상속

--- 03_blog/blogproject/blogproject/urls.py
+++ 03_blog/blogproject/blogproject/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
 import portfolio.views
 from django.conf import settings
@@ -12,9 +12,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name='home'),
-    path('blog/<int:blog_id>', blog.views.detail, name='detail'),
-    path('new/', blog.views.new, name = 'new'),
-    path('blog/create/', blog.views.create, name='create'),
+
+    path('blog/', include('blog.urls')),
 
     path('portfolio/', portfolio.views.portfolio, name='portfolio'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+

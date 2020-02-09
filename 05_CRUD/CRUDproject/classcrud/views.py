5a55a0ee92102290437e9bb0ca328f9462502f7e
numuduwer <rlatlsshwkd@gmail.com>
2020-02-10 01:33:11 +0900

    change list.html name and change object name

--- 05_CRUD/CRUDproject/classcrud/views.py
+++ 05_CRUD/CRUDproject/classcrud/views.py
@@ -8,6 +8,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView #데이
 from .models import ClassBlog
 
 class BlogView(ListView):   #html 템플릿 : 블로그 리스트를 담은 html -> (소문자 모델)_list.html
+    template_name = 'classcrud/list.html'
+    context_object_name = 'blog_list'
     model = ClassBlog
 
 class BlogCreate(CreateView): # html : form(입력공간)을 갖고있는 html -> (소문자 모델)_form.html
@@ -17,6 +19,7 @@ class BlogCreate(CreateView): # html : form(입력공간)을 갖고있는 html -
 
 
 class BlogDetail(DetailView):   # html : 상세페이지를 담은 html -> (소문자 모델)_detail.html
+    context_object_name = 'blogpost'
     model = ClassBlog
 
 class BlogUpdate(UpdateView):    # html : form(입력공간)을 갖고있는 html -> (소문자 모델)_form.html
